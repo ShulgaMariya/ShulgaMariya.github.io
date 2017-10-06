015bfea0666102c091a00268926fbe6850ed6074
ShulgaMariya <Fo-Ra-in@yandex.ua>
2017-10-06 17:02:47 +0300

    git1

---
+++ nevada/js/main.js
@@ -0,0 +1,11 @@
+/*main js*/
+wow = new WOW(
+                      {
+                      boxClass:     'wow',      // default
+                      animateClass: 'animated', // default
+                      offset:       200,          // default
+                      mobile:       false,       // default
+                      live:         false       // default
+                    }
+                    )
+                    wow.init();
